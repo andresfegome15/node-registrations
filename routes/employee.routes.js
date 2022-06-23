581fc0f7a7b96b8461f1f279d5b7e4b68b7ea4b1
andresfegome <andresfegome15@gmail.com>
2022-06-22 22:43:35 -0500

    add ruta delete

--- routes/employee.routes.js
+++ routes/employee.routes.js
@@ -1,17 +1,23 @@
-const express = require('express')
+const express = require("express");
 
 const employeeRouter = express.Router();
 
-const {getEmployees, getEmployeesbyId, createEmployee, updateExit, cancelEmployee} = require('../controller/employee.contoller')
+const {
+  getEmployees,
+  getEmployeesbyId,
+  createEmployee,
+  updateExit,
+  cancelEmployee,
+} = require("../controller/employee.contoller");
 
-employeeRouter.get('/employee',getEmployees );
+employeeRouter.get("/employee", getEmployees);
 
-employeeRouter.get('/employee/:id', getEmployeesbyId);
+employeeRouter.get("/employee/:id", getEmployeesbyId);
 
-employeeRouter.post('/employee', createEmployee);
+employeeRouter.post("/employee", createEmployee);
 
-employeeRouter.patch('/employee/:id', updateExit)
+employeeRouter.patch("/employee/:id", updateExit);
 
-employeeRouter.put('/employee/:id', cancelEmployee)
+employeeRouter.delete("/employee/:id", cancelEmployee);
 
-module.exports = {employeeRouter}
+module.exports = { employeeRouter };
